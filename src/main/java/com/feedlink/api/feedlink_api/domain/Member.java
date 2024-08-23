b1e8f0792e69c4c382a6fd263f0bbd7ed3e2a077
Minerva08 <elavl@naver.com>
2024-08-23 09:58:39 +0900

    Feat: Create 엔티티 생성

---
+++ src/main/java/com/feedlink/api/feedlink_api/domain/Member.java
@@ -0,0 +1,39 @@
+package com.feedlink.api.feedlink_api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long memberId;
+    @Column(unique = true)
+    private String memberEmail;
+    private String memberPwd;
+    @Column(unique = true)
+    private String memberAccount;
+    private String memberCode;
+    private Boolean memberStatus;
+
+    @Builder
+    public Member(Long memberId, String memberEmail, String memberPwd, String memberAccount,
+        String memberCode, Boolean memberStatus) {
+        this.memberId = memberId;
+        this.memberEmail = memberEmail;
+        this.memberPwd = memberPwd;
+        this.memberAccount = memberAccount;
+        this.memberCode = memberCode;
+        this.memberStatus = memberStatus;
+    }
+}
---
+++ src/main/java/com/feedlink/api/feedlink_api/domain/PostHashtag.java
@@ -0,0 +1,38 @@
+package com.feedlink.api.feedlink_api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post_hashtag")
+@Getter
+@NoArgsConstructor
+public class PostHashtag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long postHashtagId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="hashtag_id")
+    private Hashtag hashtag;
+
+    @Builder
+    public PostHashtag(Long postHashtagId, Post post, Hashtag hashtag) {
+        this.postHashtagId = postHashtagId;
+        this.post = post;
+        this.hashtag = hashtag;
+    }
+}
