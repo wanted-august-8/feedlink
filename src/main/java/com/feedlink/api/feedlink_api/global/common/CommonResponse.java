f1480c1627370106cf2dbb26164d5c6ba1c0982a
seoseo17 <eyyss16@gmail.com>
2024-08-23 09:57:31 +0900

    Feat: Add 글로벌 CommonResponse

---
+++ src/main/java/com/feedlink/api/feedlink_api/global/common/CommonResponse.java
@@ -0,0 +1,24 @@
+package com.feedlink.api.feedlink_api.global.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+@Getter
+public class CommonResponse<T> {
+    private final HttpStatus httpStatus;
+    private final String message;
+    private final T data;
+
+    private CommonResponse(HttpStatus status, String message, T data) {
+        this.httpStatus = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> CommonResponse<T> ok(T data) {
+        return ok(null, data);
+    }
+
+    public static <T> CommonResponse<T> ok(String message, T data) {
+        return new CommonResponse<>(HttpStatus.OK, message, data);
+    }
+}
