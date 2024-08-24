075116a6a98bf0b925d8d6fc696235c7cbbf7997
hongggs <hongseoda@gmail.com>
2024-08-24 17:53:04 +0900

    Feat: #1 Add 회원가입 중복 체크, 비밀번호 정책, 유효성 검사 관련 ErrorCode 추가
    - 회원가입 시 이메일 중복 체크를 위한 EMAIL_ALREADY_EXISTS ErrorCode 추가
    - 회원가입 시 계정 중복 체크를 위한 ACCOUNT_ALREADY_EXISTS ErrorCode 추가
    - 비밀번호 정책 위반 시 사용되는 PASSWORD_VIOLATES_POLICY ErrorCode 추가
    - 유효하지 않은 입력 값에 대한 INVALID_INPUT_VALUE ErrorCode 추가

--- src/main/java/com/feedlink/api/feedlink_api/global/error/ErrorCode.java
+++ src/main/java/com/feedlink/api/feedlink_api/global/error/ErrorCode.java
@@ -5,7 +5,12 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "사용자를 찾을 수 없습니다."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "유효하지 않은 입력 값입니다."),
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+    ACCOUNT_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 계정입니다."),
+    PASSWORD_VIOLATES_POLICY(HttpStatus.BAD_REQUEST, "비밀번호 정책에 맞지 않습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
