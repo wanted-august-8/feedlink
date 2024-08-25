4b50005b4257870f51cafd8031cf1b1d78d08a1c
Minerva08 <elavl@naver.com>
2024-08-25 13:37:37 +0900

    Refactor: Add ErrorCode 추가 - ErrorCode.DATE_VALIDATE_PARAM - ErrorCode.VALIDATE_PARAM - ErrorCode.REQUIED_PARAM

--- src/main/java/com/feedlink/api/feedlink_api/global/error/ErrorCode.java
+++ src/main/java/com/feedlink/api/feedlink_api/global/error/ErrorCode.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "사용자를 찾을 수 없습니다."),
+    DATE_VALIDATE_PARAM(HttpStatus.BAD_REQUEST, "날짜 유효성 조건에 맞지 않습니다."),
+    VALIDATE_PARAM(HttpStatus.BAD_REQUEST, "유효성 조건에 맞지 않습니다."),
+    REQUIED_PARAM(HttpStatus.INTERNAL_SERVER_ERROR, "Not Null");
 
     private final HttpStatus httpStatus;
     private final String message;
