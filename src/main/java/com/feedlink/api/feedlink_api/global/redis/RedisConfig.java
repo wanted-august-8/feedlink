b9ccb07a7d3185ac6f3fae6e9443a54258c9ef8a
perhona <perhona@kakao.com>
2024-08-29 23:36:22 +0900

    Fix: #2 Add Redis 운영환경 비밀번호 연결 설정 추가

--- src/main/java/com/feedlink/api/feedlink_api/global/redis/RedisConfig.java
+++ src/main/java/com/feedlink/api/feedlink_api/global/redis/RedisConfig.java
@@ -3,10 +3,11 @@ package com.feedlink.api.feedlink_api.global.redis;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -24,10 +25,24 @@ public class RedisConfig {
     private int port;
 
     @Bean
+    @Profile("local")
     public RedisConnectionFactory redisConnectionFactory() {
 
         log.info("Connect to Redis");
-        return new LettuceConnectionFactory(host,port);
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    @Profile("prod")
+    public RedisConnectionFactory redisConnectionFactoryProd() {
+        // RedisStandaloneConfiguration에 비밀번호 포함
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(host);
+        redisConfig.setPort(port);
+        redisConfig.setPassword(System.getenv("REDIS_PASSWORD"));  // 비밀번호 설정 추가
+
+        log.info("Connect to Redis");
+        return new LettuceConnectionFactory(redisConfig);
     }
 
     @Bean
